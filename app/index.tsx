6923202d43c385885f7b5febcb60f3c0ad6e69e2
hingfei <soonghingfei@hotmail.com>
2025-04-16 00:52:58 +0800

    config: added nativewind configs

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View className={"bg-black flex-1 justify-center items-center"}
+    >
+      <Text className={"text-4xl text-white"}>Welcome</Text>
+
+    </View>
+  );
+}
